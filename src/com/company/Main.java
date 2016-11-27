9dcd901a91e8c73f19fea6f61b7b921ed94dd608
yadanilov19 <danilov.sergey.mo04@gmail.com>
2016-11-27 13:25:58 +0500

    hi12

---
+++ src/com/company/Main.java
@@ -0,0 +1,16 @@
+package com.company;
+import jade.Boot3;
+import jade.core.*;
+
+public class Main extends Boot3 {
+
+    public Main(String[] args) {
+        super(args);
+    }
+
+    class GenerateAgentsString{
+        public GenerateAgentsString(int CountQuestionsInOneTicket, int k){
+
+        }
+    }
+}
