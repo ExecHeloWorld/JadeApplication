15f9c4f429a3513992e13e2c3d0150aa7e3cf0e1
yadanilov19 <danilov.sergey.mo04@gmail.com>
2016-12-04 13:13:12 +0500

    hi12

--- src/com/company/ManagerTickets.java
+++ src/com/company/ManagerTickets.java
@@ -1,18 +1,53 @@
 package com.company;
 import jade.core.Agent;
-import jade.core.behaviours.*;
-import jade.lang.acl.ACLMessage;
-import jade.lang.acl.MessageTemplate;
-import jade.domain.DFService;
-import jade.domain.FIPAException;
-import jade.domain.FIPAAgentManagement.DFAgentDescription;
-import jade.domain.FIPAAgentManagement.ServiceDescription;
+import jade.core.Runtime;
+import java.io.BufferedReader;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+
+import jade.wrapper.AgentContainer;
+import jade.wrapper.AgentController;
+import java.util.Scanner;
 /**
  * Created by localadmin on 26.11.2016.
  */
 public class ManagerTickets extends Agent {
-    @Override
     protected void setup() {
-        super.setup();
+        Runtime rt = Runtime.instance();
+        AgentContainer ac = getContainerController();
+
+        BufferedReader br = null;
+        int lineCount = 0;
+        try {
+            br = new BufferedReader(new InputStreamReader(new FileInputStream("startData.txt"), "utf8"));
+            String s;
+            while ((s = br.readLine()) != null) {
+                lineCount++;
+
+                String[] parametersFromString = s.split(";");
+                String nameOfAgent = parametersFromString[0];
+                String nameOfClass = "laba2." + parametersFromString[1];
+                int arg2 = Integer.valueOf(parametersFromString[2]);
+                int arg3 = Integer.valueOf(parametersFromString[3]);
+
+                try {
+                    AgentController aController = ac.createNewAgent(nameOfAgent, nameOfClass, new Object[]{nameOfAgent, arg2, arg3});
+                    aController.start();
+                } catch (Exception e) {
+                    System.out.println(e);
+                }
+            }
+        } catch (IOException ex) {
+            System.out.println("Reading error in line " + lineCount);
+            ex.printStackTrace();
+        } finally {
+            try {
+                br.close();
+            } catch (IOException ex) {
+                System.out.println("Can not close");
+            }
+        }
     }
 }
