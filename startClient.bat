d3e995e9e2d210408d83b975dffa942d6b709fc5
yadanilov19 <danilov.sergey.mo04@gmail.com>
2016-12-12 22:01:11 +0500

    bat

---
+++ startClient.bat
@@ -0,0 +1,2 @@
+java -cp lib\jade.jar;bin jade.Boot -container -host 192.168.43.216 -port 1099 -agents sol2:com.company.ManagerTickets
+@pause
---
+++ startServer.bat
@@ -0,0 +1,2 @@
+java -cp lib\jade.jar;bin jade.Boot -gui -port 1099
+@pause
